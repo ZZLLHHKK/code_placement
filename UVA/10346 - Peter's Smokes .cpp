35c38bd32e4f3fa4c5db108e88054c3153a9a258
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-04-11 20:43:04 +0800

    Create 10346 - Peter's Smokes .cpp

---
+++ UVA/10346 - Peter's Smokes .cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, k;
+
+    while(cin >> n >> k){
+        int sum = n;
+
+        while(n >= k){
+            sum += (n/k);
+            n = (n/k) + (n % k); 
+        }
+
+        cout << sum << endl;
+    }
+}
