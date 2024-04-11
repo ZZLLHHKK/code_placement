a72eb8e965c905a1cea456cb8b082b6e320fb52d
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-04-11 20:47:54 +0800

    Create 00272 - TeX Quotes.cpp

---
+++ UVA/00272 - TeX Quotes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;    
+
+int main(){
+    char c;
+    int cnt = 0;
+
+    while(cin.get(c)){
+        if(c != '"') cout << c;
+        else if(++cnt % 2) cout << "``";
+        else cout << "''";
+    }
+}
