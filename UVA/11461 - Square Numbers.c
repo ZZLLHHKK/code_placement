16c0ab3b71d9c2ceb47ef4fe05c29df8a8857257
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-04-11 20:45:34 +0800

    Create 11461 - Square Numbers.c

---
+++ UVA/11461 - Square Numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int a, b;
+	
+	while(scanf("%d%d", &a, &b) != EOF && a && b){
+		int t = floor(sqrt(b)) - ceil(sqrt(a)) + 1;
+		printf("%d\n", t);
+	}
+}
