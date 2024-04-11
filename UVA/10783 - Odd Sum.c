c66264a3d4021dbc17986f0a3b65aa0a18bef5bc
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-04-11 20:54:50 +0800

    Create 10783 - Odd Sum.c

---
+++ UVA/10783 - Odd Sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int t, j = 1;
+    scanf("%d", &t);
+
+    while(t--){
+        int a, b, sum = 0;
+        scanf("%d%d", &a, &b);
+        for(int i = a; i <= b; i++){
+            if(i % 2 == 1) sum += i;
+            else continue;
+        }
+        printf("Case %d: %d\n", j, sum);
+        j++;
+    }
+}
