bece7dd91834aeb8fbd023a3ea19b42452707851
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-04-17 11:54:04 +0800

    Update 12289 - One-Two-Three.c
    只有改排版

--- UVA/12289 - One-Two-Three.c
+++ UVA/12289 - One-Two-Three.c
@@ -8,17 +8,12 @@ int main(){
         char str[10];
         memset(str, '\0', sizeof(str));
         scanf("%s", str);
-        if(strlen(str) == 5){ 
-            printf("3\n");
-        } else{
+        if(strlen(str) == 5) printf("3\n");
+        else {
             if(strcmp("one", str) == 0) printf("1\n");
             else if(strcmp("two", str) == 0) printf("2\n");
-            else if((str[1] == 'n' && str[2] == 'e') || (str[0] == 'o' && str[2] == 'e') || (str[0] == 'o' && str[1] == 'n')){
-                printf("1\n");
-            }
-            else if((str[1] == 'w' && str[2] == 'o') || (str[0] == 't' && str[2] == 'o') || (str[0] == 't' && str[1] == 'w')){
-                printf("2\n");
-            }
+            else if((str[1] == 'n' && str[2] == 'e') || (str[0] == 'o' && str[2] == 'e') || (str[0] == 'o' && str[1] == 'n')) printf("1\n");
+            else if((str[1] == 'w' && str[2] == 'o') || (str[0] == 't' && str[2] == 'o') || (str[0] == 't' && str[1] == 'w')) printf("2\n");
         }
         memset(str, '\0', sizeof(str));
     }
