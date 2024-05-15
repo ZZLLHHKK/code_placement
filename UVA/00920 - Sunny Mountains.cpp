9f5ce633f4054345f40d7b913e37ab72855fa208
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-05-15 17:55:06 +0800

    Update 00920 - Sunny Mountains.cpp

--- UVA/00920 - Sunny Mountains.cpp
+++ UVA/00920 - Sunny Mountains.cpp
@@ -8,20 +8,20 @@ bool cmp(const p& a, const p& b) {
     return a.x < b.x;
 }
 
-class mountain {
-private:
-    int cnt;
-    vector<p> shan;
-public:
-    mountain(int ct) : cnt(ct) {
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int cnt;
+        cin >> cnt;
+        vector<p> shan;
         for (int i = 0; i < cnt; ++i) {
             p tmp;
             cin >> tmp.x >> tmp.y;
             shan.push_back(tmp);
         }
         sort(shan.begin(), shan.end(), cmp);
-    }
-    double calculate() {
         double X = 0, Y = 0, x = 0, y = 0, h = 0;
         double ans = 0.0;
         for (int i = cnt-1; i >= 0; --i) {
@@ -34,18 +34,6 @@ public:
                 h = shan[i].y;
             }
         }
-        return ans;
-    }
-};
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int ct;
-        cin >> ct;
-        mountain moun(ct);
-        cout << fixed << setprecision(2) << moun.calculate() << endl;
+        cout << fixed << setprecision(2) << ans << endl;
     }
 }
