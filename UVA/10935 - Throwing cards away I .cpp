ff2a67fa25fbcc3499fb74eb220a5b9ed1e07d27
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-05-21 16:09:03 +0800

    Update 10935 - Throwing cards away I .cpp
    UVA online judge accepted

--- UVA/10935 - Throwing cards away I .cpp
+++ UVA/10935 - Throwing cards away I .cpp
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int n;
 
-    while(cin >> n && n){
+    while (cin >> n && n) {
         queue<int> a;
         vector<int> b;
-        for(int i = 1; i <= n; i++) a.push(i);
+        for(int i = 1; i <= n; i++) 
+            a.push(i);
 
-        while(a.size() >= 2){
+        while (a.size() >= 2) {
             b.push_back(a.front());
             a.pop();
             a.push(a.front());
             a.pop();
         }
 
-        cout << "Discarded cards: ";
-        for(int j = 0; j < b.size(); j++){
-            if(j != b.size()-1) cout << b[j] << ", ";
-            else cout << b[j]; 
+        cout << "Discarded cards:";
+        if (n != 1) 
+            cout << " ";
+        for (int j = 0; j < b.size(); j++) {
+            if(j != b.size()-1) 
+                cout << b[j] << ", ";
+            else 
+                cout << b[j]; 
         }
         cout << endl;
         cout << "Remaining card: " << a.front() << endl;
