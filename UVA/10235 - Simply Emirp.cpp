a0a71b77e15a6554b34aea3a0d2180d87d73be9c
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-06-05 22:02:00 +0800

    Update 10235 - Simply Emirp.cpp
    UVA online judge accepted

--- UVA/10235 - Simply Emirp.cpp
+++ UVA/10235 - Simply Emirp.cpp
@@ -1,37 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int isPrime(int n){
-    if(n == 0 || n == 1) return 0;
-    else{
-        for(int i = 2; i <= sqrt(n); i++){
-            if(n % i == 0) return 0;
-        }
+int isPrime(int n) {
+    if (n == 0 || n == 1) 
+        return 0;
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) 
+            return 0;
     }
     return 1;
 }
 
-int rev(int n){
-    int s = 0;
-    while(n > 0){
-        s = s*10 + (n % 10);
-        n /= 10;
-    }
-    return s;
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int a;
-
-    while(cin >> a){
-        int j = isPrime(a);
-        if(j == 0) cout << a << " is not prime." << endl;
-        else if(j == 1){
-            int k = isPrime(rev(a));
-            if(k == 1 && a != rev(a)) cout << a << " is emirp." << endl;
-            else cout << a << " is prime." << endl;
+int main() {
+    string n;
+    
+    while (cin >> n) {
+        if (!isPrime(stoi(n))) 
+            cout << n << " is not prime." << endl;
+        else {
+            string tmp = n;
+            reverse(n.begin(), n.end());
+            if (isPrime(stoi(n)) && tmp != n)
+                cout << tmp << " is emirp." << endl;
+            else
+                cout << tmp << " is prime." << endl;
         }
     }
 }
