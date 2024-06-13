50c741991d68590f0599a062980e92d8b4e2cc84
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-06-13 19:45:53 +0800

    Update and rename UVA/10041 - Vito's family.cpp to UVA/1_star/10041 - Vito's family.cpp
    UVa online judge accepted

--- UVA/1_star/10041 - Vito's family.cpp
+++ UVA/1_star/10041 - Vito's family.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
+    while (t--) {
         int n, ans = 0;
         cin >> n;
         int a[n];
-        for(int i = 0; i < n; i++) cin >> a[i];
+        for(int i = 0; i < n; i++) 
+            cin >> a[i];
         sort(a, a+n);
         int m = a[n/2];
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < n; j++)
             ans += abs(a[j]-m);
-        }
         cout << ans << endl;
     }
 }
