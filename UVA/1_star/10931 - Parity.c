dca8bed2c8ef82dfcbf0febf5e76f982984f863e
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-06-14 20:21:53 +0800

    Update and rename UVA/10931 - Parity.c to UVA/1_star/10931 - Parity.c
    UVa online judge accepted

--- UVA/1_star/10931 - Parity.c
+++ UVA/1_star/10931 - Parity.c
@@ -7,18 +7,17 @@ int main(){
     while(scanf("%d", &a) && a != 0){
         int i = 0, cnt = 0;
         char str[101];
-        memset(str, '\0', sizeof(str));
         while(a > 0){
             int r = a % 2;
             a /= 2;
-            str[i] = (char)(r + '0');
-            i++;
+            str[i++] = (char)(r + '0');
         }
-        for(int j = 0; j < strlen(str); j++){
+        for(int j = 0; j < strlen(str); j++)
             if(str[j] == '1') cnt++;
-        }
+        
         printf("The parity of ");
-        for(int k = strlen(str)-1; k >= 0; k--) printf("%c", str[k]);
+        for(int k = strlen(str)-1; k >= 0; k--) 
+            printf("%c", str[k]);
         printf(" is %d (mod 2).\n", cnt);
     }
 }
