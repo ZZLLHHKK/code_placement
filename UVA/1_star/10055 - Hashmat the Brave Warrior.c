d52146aa60c89a8f106bef32650141548bf3936b
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-06-14 19:53:28 +0800

    Update and rename UVA/10055 - Hashmat the Brave Warrior.c to UVA/1_star/10055 - Hashmat the Brave Warrior.c
    UVa online judge accepted

--- UVA/1_star/10055 - Hashmat the Brave Warrior.c
+++ UVA/1_star/10055 - Hashmat the Brave Warrior.c
@@ -4,5 +4,6 @@
 int main(){
     long long int a, b;
     
-    while(scanf("%lld%lld", &a, &b) != EOF) printf("%lld\n", labs(a-b));
+    while(scanf("%lld%lld", &a, &b) != EOF) 
+        printf("%lld\n", labs(a-b));
 }
